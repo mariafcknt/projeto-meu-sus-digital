25e2c033d6bbc50144d271b15a186fb9f7842e22
Maria <mariafcknt@outlook.com>
2026-03-31 21:59:02 -0300

    initiate cns + minor fixes

---
+++ js/link-saude.js
@@ -0,0 +1,18 @@
+fetch('./links-saude.html')
+  .then(response => response.text())
+  .then(html => {
+    const container = document.getElementById('links-saude-container')
+    const limite = container.dataset.limite
+
+    const temp = document.createElement('div')
+    temp.innerHTML = html
+
+    if (limite) {
+      const cards = temp.querySelectorAll('.links-content-wrapper')
+      cards.forEach((card, index) => {
+        if (index >= parseInt(limite)) card.remove()
+      })
+    }
+
+    container.innerHTML = temp.innerHTML
+})
---
+++ js/navbar.js
@@ -0,0 +1,26 @@
+fetch('./navbar.html')
+  .then(response => response.text())
+  .then(html => {
+    document.getElementById('navbar-container').innerHTML = html
+
+    // marca o item ativo após carregar
+    const navItems = document.querySelectorAll('.navbar-item')
+    let currentPath = window.location.pathname
+
+    if (currentPath.endsWith('/') || currentPath === '') {
+      currentPath = '/index.html'
+    }
+
+    navItems.forEach(item => {
+      const itemHref = item.getAttribute('href')
+      const itemPath = itemHref.replace('./', '/')
+
+      if (currentPath.endsWith(itemPath)) {
+        item.classList.add('active')
+        item.setAttribute('aria-current', 'page')
+      } else {
+        item.classList.remove('active')
+        item.removeAttribute('aria-current')
+      }
+    })
+  })
